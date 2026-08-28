30f2466fbaad604670b3663134729047824e4a85
jupitters <sampaiobale@gmail.com>
2026-08-28 13:16:09 -0300

    auth controller

---
+++ src/main/java/com/jupitters/book_network/auth/AuthenticationController.java
@@ -0,0 +1,23 @@
+package com.jupitters.book_network.auth;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+@Tag(name = "Authentication")
+public class AuthenticationController {
+    private final AuthenticationService authService;
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public ResponseEntity<?> register(@RequestBody @Valid RegistrationRequest request) {
+        authService.register(request);
+        return ResponseEntity.accepted().build();
+    }
+}
