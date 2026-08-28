74ef5681c7ea17a0fcd456a751b21fd3ae1a22e5
jupitters <sampaiobale@gmail.com>
2026-08-28 01:17:08 -0300

    token repo

---
+++ src/main/java/com/jupitters/book_network/repository/TokenRepository.java
@@ -0,0 +1,10 @@
+package com.jupitters.book_network.repository;
+
+import com.jupitters.book_network.model.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TokenRepository extends JpaRepository<Token, Integer> {
+    Optional<Token> findByToken(String token);
+}
