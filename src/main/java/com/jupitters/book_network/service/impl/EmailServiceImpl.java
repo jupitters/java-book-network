c9bf005ef7e9eb21a655b42ad1cb2b6f73d9443c
jupitters <sampaiobale@gmail.com>
2026-08-29 17:47:46 -0300

    email service

--- src/main/java/com/jupitters/book_network/service/impl/EmailServiceImpl.java
+++ src/main/java/com/jupitters/book_network/service/impl/EmailServiceImpl.java
@@ -4,10 +4,21 @@ import com.jupitters.book_network.model.User;
 import com.jupitters.book_network.service.EmailService;
 import com.jupitters.book_network.service.TokenService;
 import com.jupitters.book_network.template.EmailTemplateName;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.springframework.mail.javamail.MimeMessageHelper.MULTIPART_MODE_MIXED;
 
 @Service
 @RequiredArgsConstructor
@@ -21,7 +32,38 @@ public class EmailServiceImpl implements EmailService {
         String newToken = tokenService.generateAndSaveActivationToken(user);
     }
 
-    public void sendEmail(String to, String username, EmailTemplateName emailTemplate, String confirmationUrl, String confirmationCode, String subject){
+    @Async
+    public void sendEmail(String to, String username, EmailTemplateName emailTemplate, String confirmationUrl, String confirmationCode, String subject) throws MessagingException {
+        String templateName;
+        if(emailTemplate == null) {
+            templateName = "confirm-email";
+        } else {
+            templateName = emailTemplate.name();
+        }
 
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(
+                mimeMessage,
+                MULTIPART_MODE_MIXED,
+                UTF_8.name()
+        );
+
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("username", username);
+        properties.put("confirmationUrl", confirmationUrl);
+        properties.put("confirmation_code", confirmationCode);
+
+        Context context = new Context();
+        context.setVariables(properties);
+
+        helper.setFrom("contact@jupitters.com");
+        helper.setTo(to);
+        helper.setSubject(subject);
+
+        String template = templateEngine.process(templateName, context);
+
+        helper.setText(template, true);
+
+        mailSender.send(mimeMessage);
     }
 }
