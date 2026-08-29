dd698cb5608de902bfa061453472e526629384e7
jupitters <sampaiobale@gmail.com>
2026-08-29 17:21:37 -0300

    token service

--- src/main/java/com/jupitters/book_network/service/impl/TokenServiceImpl.java
+++ src/main/java/com/jupitters/book_network/service/impl/TokenServiceImpl.java
@@ -1,19 +1,31 @@
 package com.jupitters.book_network.service.impl;
 
+import com.jupitters.book_network.model.Token;
 import com.jupitters.book_network.model.User;
+import com.jupitters.book_network.repository.TokenRepository;
 import com.jupitters.book_network.service.TokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.security.SecureRandom;
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class TokenServiceImpl implements TokenService {
+    private final TokenRepository tokenRepository;
+
     @Override
     public String generateAndSaveGenerationToken(User user) {
         String generatedToken = generateActivationCode(6);
-        return "";
+        Token token = Token.builder()
+                .token(generatedToken)
+                .createdAt(LocalDateTime.now())
+                .expiresAt(LocalDateTime.now().plusMinutes(15))
+                .user(user)
+                .build();
+        tokenRepository.save(token);
+        return generatedToken;
     }
 
     private String generateActivationCode(int length) {
