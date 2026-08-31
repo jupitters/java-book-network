bcb1ca331eb0cfd24898832f5a52ea3e619a0151
jupitters <sampaiobale@gmail.com>
2026-08-31 13:12:03 -0300

    adding auditing entity listener

--- src/main/java/com/jupitters/book_network/model/Book.java
+++ src/main/java/com/jupitters/book_network/model/Book.java
@@ -6,6 +6,7 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -15,6 +16,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Book {
     @Id
     @GeneratedValue
