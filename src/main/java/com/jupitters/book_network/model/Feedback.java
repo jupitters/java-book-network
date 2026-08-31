85dbe75b62dd1d4fbb7cfac2c4d23c248b4906e8
jupitters <sampaiobale@gmail.com>
2026-08-31 13:11:20 -0300

    feedbakc entity

--- src/main/java/com/jupitters/book_network/model/Feedback.java
+++ src/main/java/com/jupitters/book_network/model/Feedback.java
@@ -1,10 +1,16 @@
 package com.jupitters.book_network.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Feedback {
     @Id
     @GeneratedValue
@@ -12,4 +18,20 @@ public class Feedback {
 
     private Double note;
     private String comment;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime updatedAt;
+
+    @CreatedBy
+    @Column(nullable = false, updatable = false)
+    private Integer createdBy;
+
+    @LastModifiedBy
+    @Column(insertable = false)
+    private Integer updatedBy;
 }
