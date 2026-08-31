3a6cc0a2d96c44de327229687760feaac36705b5
jupitters <sampaiobale@gmail.com>
2026-08-31 19:32:44 -0300

    audit aware

--- src/main/java/com/jupitters/book_network/config/BeansConfig.java
+++ src/main/java/com/jupitters/book_network/config/BeansConfig.java
@@ -3,6 +3,7 @@ package com.jupitters.book_network.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -31,5 +32,10 @@ public class BeansConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) {
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuditorAware<Integer> auditorAware() {
+        return new ApplicationAuditAware();
     }
 }
