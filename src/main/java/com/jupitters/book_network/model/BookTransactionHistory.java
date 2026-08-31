4022f878c01783e155027dbb72205bf07a4ffe9c
jupitters <sampaiobale@gmail.com>
2026-08-31 13:32:23 -0300

    book transaction history

---
+++ src/main/java/com/jupitters/book_network/model/BookTransactionHistory.java
@@ -0,0 +1,19 @@
+package com.jupitters.book_network.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class BookTransactionHistory extends BaseEntity {
+    private boolean returned;
+    private boolean returnApproved;
+}
