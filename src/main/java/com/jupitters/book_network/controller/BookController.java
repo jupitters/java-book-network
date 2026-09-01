aec1d89593b9b0466802f285165b0ba914249d4c
jupitters <sampaiobale@gmail.com>
2026-09-01 15:10:56 -0300

    book controller

---
+++ src/main/java/com/jupitters/book_network/controller/BookController.java
@@ -0,0 +1,24 @@
+package com.jupitters.book_network.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/book")
+@RequiredArgsConstructor
+@Tag(name = "Book")
+public class BookController {
+    private final BookService bookService;
+
+    @PostMapping
+    public ResponseEntity<Integer> saveBook(@Valid  @RequestBody BookRequest request, Authentication connectedUser) {
+        return ResponseEntity.ok(bookService.saveBook(request, connectedUser));
+    }
+}
