31b0d7c23ff48afc22ec19cee9f6ce3f07854eb1
jupitters <sampaiobale@gmail.com>
2026-09-01 16:07:17 -0300

    book service

---
+++ src/main/java/com/jupitters/book_network/service/impl/BookServiceImpl.java
@@ -0,0 +1,10 @@
+package com.jupitters.book_network.service.impl;
+
+import com.jupitters.book_network.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BookServiceImpl implements BookService {
+}
