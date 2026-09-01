eb52ec88de4cb139a0522ce59011eadecf407e2a
jupitters <sampaiobale@gmail.com>
2026-09-01 18:36:37 -0300

    book service

--- src/main/java/com/jupitters/book_network/service/BookService.java
+++ src/main/java/com/jupitters/book_network/service/BookService.java
@@ -1,5 +1,6 @@
 package com.jupitters.book_network.service;
 
+import com.jupitters.book_network.dto.BookRequest;
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.Authentication;
 
--- src/main/java/com/jupitters/book_network/service/impl/BookServiceImpl.java
+++ src/main/java/com/jupitters/book_network/service/impl/BookServiceImpl.java
@@ -1,5 +1,8 @@
 package com.jupitters.book_network.service.impl;
 
+import com.jupitters.book_network.dto.BookRequest;
+import com.jupitters.book_network.model.Book;
+import com.jupitters.book_network.model.User;
 import com.jupitters.book_network.service.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
@@ -8,8 +11,26 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
+    private final BookRepository bookRepository;
+
     @Override
     public Integer saveBook(BookRequest request, Authentication connectedUser) {
+        User user = (User) connectedUser.getPrincipal();
+        Book book = toBook(request);
+        book.setOwner(user);
+        bookRepository.save(book);
+
         return 0;
+    }
+
+    private Book toBook(BookRequest request) {
+        return Book.builder()
+                .id(request.id())
+                .title(request.title())
+                .authorName(request.authorName())
+                .synopsis(request.synopsys())
+                .archived(false)
+                .shareable(request.shareable())
+                .build();
     }
 }
