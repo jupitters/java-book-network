cfac1ca6df482b6ecbaa1c60abe54752f61e455d
jupitters <sampaiobale@gmail.com>
2026-09-02 15:07:34 -0300

    get book by id service

--- src/main/java/com/jupitters/book_network/service/BookService.java
+++ src/main/java/com/jupitters/book_network/service/BookService.java
@@ -1,9 +1,11 @@
 package com.jupitters.book_network.service;
 
 import com.jupitters.book_network.dto.BookRequest;
+import com.jupitters.book_network.dto.BookResponse;
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.Authentication;
 
 public interface BookService {
     Integer saveBook(BookRequest request, Authentication connectedUser);
+    BookResponse findById(Integer bookId);
 }
--- src/main/java/com/jupitters/book_network/service/impl/BookServiceImpl.java
+++ src/main/java/com/jupitters/book_network/service/impl/BookServiceImpl.java
@@ -1,10 +1,12 @@
 package com.jupitters.book_network.service.impl;
 
 import com.jupitters.book_network.dto.BookRequest;
+import com.jupitters.book_network.dto.BookResponse;
 import com.jupitters.book_network.model.Book;
 import com.jupitters.book_network.model.User;
 import com.jupitters.book_network.repository.BookRepository;
 import com.jupitters.book_network.service.BookService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
@@ -21,6 +23,30 @@ public class BookServiceImpl implements BookService {
         book.setOwner(user);
 
         return bookRepository.save(book).getId();
+    }
+
+    @Override
+    public BookResponse findById(Integer bookId){
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new EntityNotFoundException("Book not found"));
+
+        return toBookResponse(book);
+
+    }
+
+    private BookResponse toBookResponse(Book book) {
+        return BookResponse.builder()
+                .id(book.getId())
+                .title(book.getTitle())
+                .authorName(book.getAuthorName())
+                .isbn(book.getIsbn())
+                .synopsys(book.getSynopsis())
+                .rate(book.getRate())
+                .archived(book.isArchived())
+                .shareable(book.isShareable())
+                .owner(book.getOwner().getFullName())
+                //.cover
+                .build();
     }
 
     private Book toBook(BookRequest request) {
