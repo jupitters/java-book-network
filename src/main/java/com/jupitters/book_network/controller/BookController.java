8618e58dc62963a73db02fc316d789d6b7ba6f61
jupitters <sampaiobale@gmail.com>
2026-09-02 15:09:50 -0300

    get book by id controller

--- src/main/java/com/jupitters/book_network/controller/BookController.java
+++ src/main/java/com/jupitters/book_network/controller/BookController.java
@@ -1,16 +1,14 @@
 package com.jupitters.book_network.controller;
 
 import com.jupitters.book_network.dto.BookRequest;
+import com.jupitters.book_network.dto.BookResponse;
 import com.jupitters.book_network.service.BookService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/book")
@@ -22,5 +20,10 @@ public class BookController {
     @PostMapping
     public ResponseEntity<Integer> saveBook(@Valid @RequestBody BookRequest request, Authentication connectedUser) {
         return ResponseEntity.ok(bookService.saveBook(request, connectedUser));
+    }
+
+    @GetMapping("{bookId}")
+    public ResponseEntity<BookResponse> findBookById(@PathVariable Integer bookId) {
+        return ResponseEntity.ok(bookService.findById(bookId));
     }
 }
