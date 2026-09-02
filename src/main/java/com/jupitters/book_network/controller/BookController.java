b162bd629dc63f3cf4c05c8db273f8858a7c3c0a
jupitters <sampaiobale@gmail.com>
2026-09-02 15:37:33 -0300

    book controller

--- src/main/java/com/jupitters/book_network/controller/BookController.java
+++ src/main/java/com/jupitters/book_network/controller/BookController.java
@@ -26,4 +26,13 @@ public class BookController {
     public ResponseEntity<BookResponse> findBookById(@PathVariable Integer bookId) {
         return ResponseEntity.ok(bookService.findById(bookId));
     }
+
+    @GetMapping
+    public ResponseEntity<PageResponse<BookResponse>> findAllBooks(
+            @RequestParam(name = "page", defaultValue = "0", required = false) int page,
+            @RequestParam(name = "size", defaultValue = "10", required = false) int size,
+            Authentication connectedUser
+    ) {
+        return ResponseEntity.ok(bookService.findAllBooks(page, size, connectedUser));
+    }
 }
