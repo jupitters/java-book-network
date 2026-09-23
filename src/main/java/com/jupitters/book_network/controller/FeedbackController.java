0fca8152b837e5bc125fdd66f798ff5d8f38b3b5
jupitters <sampaiobale@gmail.com>
2026-09-23 19:08:33 -0300

    feedbakc request

--- src/main/java/com/jupitters/book_network/controller/FeedbackController.java
+++ src/main/java/com/jupitters/book_network/controller/FeedbackController.java
@@ -2,7 +2,12 @@ package com.jupitters.book_network.controller;
 
 import com.jupitters.book_network.service.FeedbackService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Feedbacks")
 public class FeedbackController {
     private final FeedbackService feedbackService;
+
+    @PostMapping
+    public ResponseEntity<Integer> saveFeedback(
+            @Valid @RequestBody FeedbackRequest request,
+            Authentication connectedUser
+    ) {
+        return ResponseEntity.ok(feedbackService.saveFeedback(request, connectedUser));
+    }
 }
--- src/main/java/com/jupitters/book_network/service/FeedbackService.java
+++ src/main/java/com/jupitters/book_network/service/FeedbackService.java
@@ -1,4 +1,8 @@
 package com.jupitters.book_network.service;
 
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.Authentication;
+
 public interface FeedbackService {
+    Integer saveFeedback(FeedbackRequest request, Authentication connectedUser);
 }
