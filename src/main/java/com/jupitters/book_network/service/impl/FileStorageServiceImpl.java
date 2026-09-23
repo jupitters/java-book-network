956f1644b30ab07b92c7e527da11c4eb6e4eca63
jupitters <sampaiobale@gmail.com>
2026-09-23 18:45:26 -0300

    file storage service

--- src/main/java/com/jupitters/book_network/service/impl/FileStorageServiceImpl.java
+++ src/main/java/com/jupitters/book_network/service/impl/FileStorageServiceImpl.java
@@ -2,16 +2,70 @@ package com.jupitters.book_network.service.impl;
 
 import com.jupitters.book_network.model.Book;
 import com.jupitters.book_network.service.FileStorageService;
+import jakarta.validation.Valid;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jspecify.annotations.Nullable;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class FileStorageServiceImpl implements FileStorageService {
+    @Value("${application.file.upload.photos-output-path}")
+    private String fileUploadPath;
+
     @Override
-    public String saveFile(MultipartFile file, Book book, Integer userId) {
-        return "";
+    public String saveFile(
+            @NonNull MultipartFile sourceFile,
+            @NonNull Book book,
+            @NonNull Integer userId) {
+        final String fileUploadSubpath = "users" + File.separator + userId;
+        return uploadFile(sourceFile, fileUploadSubpath);
+    }
+
+    private String uploadFile(@NonNull MultipartFile sourceFile, @NonNull String fileUploadSubpath) {
+        final String finalUploadPath = fileUploadPath + File.separator + fileUploadSubpath;
+        File targetFolder = new File(finalUploadPath);
+        if(!targetFolder.exists()) {
+            boolean folderCreated = targetFolder.mkdirs();
+            if(!folderCreated) {
+                log.warn("Failed to create the target folder.");
+                return null;
+            }
+        }
+        final String fileExtension = getFileExtension(sourceFile.getOriginalFilename());
+        String targetFilePath = finalUploadPath + File.separator + System.currentTimeMillis() + "." + fileExtension;
+        Path targetPath = Paths.get(targetFilePath);
+        try {
+            Files.write(targetPath, sourceFile.getBytes());
+            log.info("File saved to ", targetFilePath);
+            return targetFilePath;
+        } catch (IOException e) {
+            log.error("File was not saved", e);
+        }
+
+        return null;
+    }
+
+    private String getFileExtension(@Nullable String fileName) {
+        if(fileName == null || fileName.isEmpty()) {
+            return null;
+        }
+        int lastDotIndex = fileName.lastIndexOf('.');
+        if(lastDotIndex == -1) {
+            return null;
+        }
+        return fileName.substring(lastDotIndex + 1).toLowerCase();
     }
 }
