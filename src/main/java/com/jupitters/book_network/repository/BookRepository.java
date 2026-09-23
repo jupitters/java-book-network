d8d30de4618105f83a28b8e1fd3974fec901d01e
jupitters <sampaiobale@gmail.com>
2026-09-23 02:58:38 -0300

    book repo

--- src/main/java/com/jupitters/book_network/repository/BookRepository.java
+++ src/main/java/com/jupitters/book_network/repository/BookRepository.java
@@ -1,7 +1,17 @@
 package com.jupitters.book_network.repository;
 
 import com.jupitters.book_network.model.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
+    @Query("""
+            SELECT b from Book b 
+            WHERE b.archived = false 
+            AND b.shareable = true
+            AND b.owner.id != :userId
+            """)
+    Page<Book> findAllDisplayableBooks(Pageable pageable, Integer userId);
 }
