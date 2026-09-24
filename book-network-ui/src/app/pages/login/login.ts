9a1655c82f83392960885d1c1698c61fd0fcf00e
jupitters <sampaiobale@gmail.com>
2026-09-24 20:27:23 -0300

    login

--- book-network-ui/src/app/pages/login/login.ts
+++ book-network-ui/src/app/pages/login/login.ts
@@ -1,6 +1,6 @@
 // @ts-ignore
 
-import {Component, inject} from '@angular/core';
+import {ChangeDetectorRef, Component, inject} from '@angular/core';
 import {AuthenticationRequest} from '../../services/models/authentication-request';
 import {FormsModule} from '@angular/forms';
 import {Router} from '@angular/router';
@@ -22,7 +22,8 @@ export class Login {
 
   constructor(
     private router: Router,
-    private api: Api
+    private api: Api,
+    private cdr: ChangeDetectorRef
   ) {
   }
 
@@ -36,8 +37,22 @@ export class Login {
       const res = await this.api.invoke(authenticate, params);
       // save token
       this.router.navigate(['books']);
-    } catch (err) {
+    } catch (err: any) {
       console.log(err);
+
+      let error = err.error;
+
+      if (error instanceof Blob) {
+        error = JSON.parse(await error.text());
+      }
+      if (error?.validationErrors) {
+        this.errorMsg = error.validationErrors;
+      } else if (error?.error) {
+        this.errorMsg = [error.error];
+      } else {
+        this.errorMsg = ['Erro ao fazer login'];
+      }
+      this.cdr.detectChanges();
     }
 
   }
